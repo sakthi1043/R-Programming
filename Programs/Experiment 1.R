9707c78371de5fd50f0ca180b3425bd307830bd5
sakthi1043 <sakthi1043@gmail.com>
2025-07-23 12:19:50 +0530

    First Commit

---
+++ Programs/Experiment 1.R
@@ -0,0 +1,18 @@
+cat("Enter the values separated by space:")
+val<-scan()
+
+cat(val)
+
+x<-c("A","B","C","D","E")
+y<-c(1,2,3,4,5)
+barplot(y,names.arg = x,col = blues9)
+
+pie(y)
+
+c(10:50)
+
+seq(50,100)
+
+2*5+10-2**4/2
+
+q()
---
+++ Programs/Experiment 2.R
@@ -0,0 +1,12 @@
+name1<-"sakthi"
+print(name1)
+name2<-"vel"
+name3<-"Hari"
+name4<-"Arun"
+name5<-"Surya"
+ls()
+
+rm(list=ls())
+ls()
+
+q()
